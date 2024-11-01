d33f6f81f6b7ed679fe573431a0f44819c5af1c4
Pedro Arthur <pedroarthurmelorodrigues@gmail.com>
2024-11-01 11:32:25 -0300

    Create trabalhobase.rs

---
+++ trabalhobase.rs
@@ -0,0 +1,113 @@
+/////////////////////Arquivo//////////////////////////
+struct Arquivo {
+    
+}
+
+impl Arquivo{
+
+    fn new(){
+
+    }
+
+    fn alterar_permissao() {
+
+    }
+    
+    fn stat(){
+
+    }
+
+
+}
+/////////////////////Permissao//////////////////////////
+struct Permissao {
+    
+}
+impl Permissao {
+    
+    fn new() {
+
+    }
+
+    fn octal(){
+
+    }
+
+}
+
+/////////////////////Diretório//////////////////////////
+struct Diretorio{
+    
+}
+impl Diretorio {
+
+    fn new(){
+
+    }
+
+    fn adiciona_arquivo(){
+
+    }
+
+    fn remove_arquivo(){
+
+    }
+
+    fn listar_conteudo(){
+
+    }
+
+}
+/////////////////////Usuario//////////////////////////
+struct Usuario {
+    
+}
+impl Usuario{
+
+    fn new(){
+
+    }
+
+    fn adiciona_grupo(){
+
+    }
+    
+    fn remove_grupo(){
+
+    }
+
+    fn listar_grupos(){
+
+    }
+
+}
+/////////////////////Grupo//////////////////////////
+struct Grupo {
+    
+}
+
+impl Grupo {
+
+    fn new(){
+
+    }
+
+    fn adiciona_membro(){
+
+    }
+
+    fn remove_membro(){
+
+    }
+
+    fn listar_grupos(){
+
+    }
+
+}
+
+
+
+fn main(){
+
+}
